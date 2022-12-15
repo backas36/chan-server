7e7cbad096275432893cbdd2d1d1ae3e6c599512
backas36 <backas36@gmail.com>
2022-12-16 01:13:02 +0800

    update origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://127.0.0.1:3000", "http://localhost:3000"]
+const allowedOrigins = ["https://chan-web.onrender.com","http://127.0.0.1:3000", "http://localhost:3000"]
 
 module.exports = allowedOrigins
